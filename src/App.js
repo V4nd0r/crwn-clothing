7bba31c7dce5f02c5fc8311ed7ddd267d6170268
Hajnalka Krekuska <krekuskahajni@freemail.hu>
2023-04-15 19:16:13 +0100

    Marking up categories

--- src/App.js
+++ src/App.js
@@ -1,7 +1,47 @@
 
 const App = () => {
   return (
-    <div>0.0</div> 
+    <div className="categories__container">
+      <div className="categories__card">
+      0.0
+        <div className="categories__card__label">
+          <h2>Hats</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+
+      <div className="categories__card">
+      0.0
+        <div className="categories__card__label">
+          <h2>Jackets</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+
+      <div className="categories__card">
+      0.0
+        <div className="categories__card__label">
+          <h2>Sneakers</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+
+      <div className="categories__card">
+      0.0
+        <div className="categories__card__label">
+          <h2>Womens</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+
+      <div className="categories__card">
+      0.0
+        <div className="categories__card__label">
+          <h2>Mens</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+    </div>  
   )
 };
 
