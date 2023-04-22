69ad8ebd2468b5bd3c8b22e6ede1fe0fdfcf62c4
Hajnalka Krekuska <krekuskahajni@freemail.hu>
2023-04-22 13:48:33 +0100

    added firestore

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import {getFirestore, doc, getDoc, setDoc} from 'firebase/firestore';
 
 //Firebase configuration
 const firebaseConfig = {
@@ -11,7 +12,7 @@ const firebaseConfig = {
     appId: "1:135137139:web:ac0669a7dc6ce6474e2706"
   };
 
-  // Initialize Firebase
+// Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 
 const provider = new GoogleAuthProvider();
@@ -19,5 +20,36 @@ const provider = new GoogleAuthProvider();
     prompt: "select_account"
   });
 
+  export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+    console.log (userDocRef);
+
+    const userSnapshot = await getDoc(userDocRef);
+    console.log(userSnapshot);
+    console.log(userSnapshot.exists());
+
+    //CHECK IF USER DATA EXISTS
+    //IF USER DATA DOES NOT EXIST: create/set document with data from userAuth in my collection
+    if(!userSnapshot.exists()) {
+      const {displayName, email} =userAuth;
+      const createdAt = new Date();
+
+      try {
+        await setDoc(userDocRef, {
+          displayName,
+          email,
+          createdAt
+        });
+      } catch (error) {
+        console.log('error crearing the user', error.message);
+      }
+    }
+    //IF USER DATA EXISTS: return userDocRef
+    return userDocRef;
+  };
+
   export const auth = getAuth();
   export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+  export const db = getFirestore();
+
+
