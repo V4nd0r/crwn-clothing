88ea10a5738e2cc6a1b91ccf1386b172843e2df1
Hajnalka Krekuska <krekuskahajni@freemail.hu>
2023-04-28 16:27:31 +0100

    adding cart dropdown element

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -2,6 +2,7 @@ import { Fragment, useContext } from "react";
 import { Link, Outlet } from "react-router-dom";
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 import { UserContext } from "../../contexts/user.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 import './navigation.styles.scss';
@@ -31,6 +32,7 @@ const Navigation = () => {
 
             <CartIcon/>
           </div>
+          <CartDropdown/>
         </div>
         <Outlet/>
       </Fragment>
