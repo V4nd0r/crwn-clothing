203574bce53aa985d1acd5400c10403d276495bd
Hajnalka Krekuska <krekuskahajni@freemail.hu>
2023-04-29 18:20:21 +0100

    adding cart context

---
+++ src/contexts/cart.context.jsx
@@ -0,0 +1,14 @@
+import { createContext, useState} from "react";
+
+export const CartContext = createContext({
+    isCartOpen: false,
+    setIsCartOpen: () => {}
+})
+
+export const CartProvider = ({childen}) => {
+    const [isCartOpen, setIsCartOpen] = useState(false);
+    const value = {isCartOpen, setIsCartOpen};
+    return(
+        <CartContext.Provider value={value}> {childen} </CartContext.Provider>
+    )
+}
