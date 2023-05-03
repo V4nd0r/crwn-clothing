ed13bc34ff3e7904bc793ad529bcefd0e86cdbab
Hajnalka Krekuska <krekuskahajni@freemail.hu>
2023-05-03 16:31:17 +0100

    Revert "Add items to cart pt. 2"
    This reverts commit c99db5012be3ee72e22ea2145b2c8cd6e74a558b.

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -2,20 +2,10 @@ import { createContext, useState} from "react";
 
 const addCartItem = (cartItems, productToAdd) => {
     //see if cartItems contains productToAdd
-    const existingCartItem = cartItems.find(
-        (cartItem) => cartItem.id === productToAdd.id
-        );
 
     //If found increment quantity
-    if(existingCartItem) {
-        return cartItems.map((cartItem) => cartItem.id === productToAdd.id 
-        ? {...cartItem, quantity: cartItem.quantity++}
-        : cartItem
-        )
-    }
 
     //return new array with modified cartItems/ new cart item
-    return [...cartItems, {...productToAdd, quantity: 1 }];
 }
 
 export const CartContext = createContext({
@@ -33,7 +23,7 @@ export const CartProvider = ({childen}) => {
         setCartItems(addCartItem (cartItems, productToAdd));
     }
 
-    const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems };
+    const value = { isCartOpen, setIsCartOpen };
     return <CartContext.Provider value={value}>{childen}</CartContext.Provider>;
 
 };
