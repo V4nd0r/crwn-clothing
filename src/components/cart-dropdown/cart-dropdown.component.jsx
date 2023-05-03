d036d0ea114f0697273f9a2ca38756b51a03e27c
Hajnalka Krekuska <krekuskahajni@freemail.hu>
2023-05-03 16:31:27 +0100

    Revert "add items to cart pt 1"
    This reverts commit 0ea896e6929e609906dfbfc680f833d6a6a108a1.

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,10 @@
 import Button from '../button/button.component';
-import CartItem from '../cart-item/cart-item.component';
 import "./cart-dropdown.styles.scss";
 
 const CartDropdown = () => {
     return(
         <div className="cart-dropdown-container">
-            <div className="cart-items">
-                {[].map(item => <CartItem cartItem={item}/>)}
-            </div>
+            <div className="cart-items"></div>
             <Button>CHECK OUT</Button>
         </div>
     )
--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,29 +1,13 @@
 import { createContext, useState} from "react";
 
-const addCartItem = (cartItems, productToAdd) => {
-    //see if cartItems contains productToAdd
-
-    //If found increment quantity
-
-    //return new array with modified cartItems/ new cart item
-}
-
 export const CartContext = createContext({
     isCartOpen: false,
     setIsOpen: () => {},
-    cartItems: [],
-    addItemToCart: () => {}
 });
 
 export const CartProvider = ({childen}) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
-    const [cartItems, setCartItems] =useState([]);
-
-    const addItemToCart = (productToAdd) => {
-        setCartItems(addCartItem (cartItems, productToAdd));
-    }
-
-    const value = { isCartOpen, setIsCartOpen };
-    return <CartContext.Provider value={value}>{childen}</CartContext.Provider>;
+    const value = {isCartOpen, setIsCartOpen};
+    return <CartContext.Provider value={value}> {childen} </CartContext.Provider>;
 
 };
