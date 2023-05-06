89bf287813c47e4967bf1ea60a8ac8ab2e13c1a7
Hajnalka Krekuska <krekuskahajni@freemail.hu>
2023-05-06 12:00:25 +0100

    cart icon and cart item to styled component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,7 +3,10 @@ import { useNavigate } from 'react-router-dom';
 import { CartContext } from '../../contexts/cart.context';
 import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
-import "./cart-dropdown.styles.scss";
+import {CartDrpdownContainer, 
+        EmptyMessage, 
+        CartItems} 
+        from "./cart-dropdown.styles.jsx";
 
 
 const CartDropdown = () => {
@@ -15,18 +18,18 @@ const CartDropdown = () => {
     };
 
     return (
-        <div className='cart-dropdown-container'>
-        <div className='cart-items'>
+        <CartDrpdownContainer>
+        <CartItems>
             {cartItems.length ? (
             cartItems.map((cartItem) => (
                 <CartItem key={cartItem.id} cartItem={cartItem} />
             ))
             ) : (
-            <span className='empty-message'>Your cart is empty</span>
+            <EmptyMessage>Your cart is empty</EmptyMessage>
             )}
-        </div>
+        </CartItems>
         <Button onClick = {goToChekoutHandler}>GO TO CHECKOUT</Button>
-        </div>
+        </CartDrpdownContainer>
     );
     };
 
