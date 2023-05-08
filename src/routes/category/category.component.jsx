befb992ebb97f2e154cb4da8e725fbf49d50399a
Hajnalka Krekuska <krekuskahajni@freemail.hu>
2023-05-08 15:33:27 +0100

    Revert "Temporary"
    This reverts commit 2edf4b3d33ab822ed46be93c0ce0477adeb05fb0.

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,31 +1,30 @@
 import { useState, useEffect, Fragment } from 'react';
-import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectCategoriesMap } from '../../store/categories/category.selector'; 
 import ProductCard from '../../components/product-card/product-card.component';
-import { selectCategoriesMap } from '../../store/categories/categories.selector';
-import { CategoryContainer, Title } from './category.style';
+import  {CategoryContainer, CategoryTitle} from './category.style.jsx';
 
 const Category = () => {
-  const { category } = useParams();
-  const categoriesMap = useSelector(selectCategoriesMap);
-  console.log('rendering category');
-  const [products, setProducts] = useState(categoriesMap[category]);
+    const { category } = useParams();
+    const categoriesMap = useSelector(selectCategoriesMap);
+    const [products, setProducts] = useState(categoriesMap[category]);
 
-  useEffect(() => {
-    setProducts(categoriesMap[category]);
-  }, [category, categoriesMap]);
+    useEffect(() => {
+        setProducts(categoriesMap[category]);
+    }, [category, categoriesMap])
 
-  return (
-    <Fragment>
-      <Title>{category.toUpperCase()}</Title>
-      <CategoryContainer>
-        {products &&
-          products.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-      </CategoryContainer>
-    </Fragment>
-  );
+    return (
+        <Fragment>
+            <CategoryTitle>{category.toUpperCase()}</CategoryTitle>
+            <CategoryContainer>
+                {products && 
+                products.map((product) => (
+                <ProductCard key={product.id} product={product} /> 
+                ))}
+            </CategoryContainer>
+        </Fragment>
+    )
 };
 
 export default Category;
