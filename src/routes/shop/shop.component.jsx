39eb399cdad3766f6615e5dc6d75442afc96e943
Hajnalka Krekuska <krekuskahajni@freemail.hu>
2023-05-24 10:55:32 +0100

    redux thunk pt2

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,28 +1,23 @@
-import { useEffect } from "react";
-import { Route, Routes } from "react-router-dom"; 
-import { useDispatch } from "react-redux"
-import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
-import { setCategories } from "../../store/categories/category.action";
-import CategoriesPreview from "../categories-preview/categories-preview.component";
-import Category from "../category/category.component";
+import { useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import CategoriesPreview from '../categories-preview/categories-preview.component';
+import Category from '../category/category.component';
+import { fetchCategoriesStartAsync } from '../../store/categories/category.action';
 
 const Shop = () => {
-    const dispatch = useDispatch();
-    
-    useEffect(() => {
-        const getCategoriesMap = async () => {
-            const categoriesArray = await getCategoriesAndDocuments('categories');
-            dispatch (setCategories(categoriesArray));
-        }; 
-        getCategoriesMap();
-    }, []);
+  const dispatch = useDispatch();
 
-    return (
-        <Routes>
-            <Route index element={<CategoriesPreview />} />
-            <Route path=':category' element={<Category />} />
-        </Routes>
-    );
+  useEffect(() => {
+    dispatch(fetchCategoriesStartAsync());
+  }, []);
+
+  return (
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+      <Route path=':category' element={<Category />} />
+    </Routes>
+  );
 };
 
 export default Shop;
