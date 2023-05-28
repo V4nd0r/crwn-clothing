fff51ae1cef1f643cc8c4ffd0310b6a0189f4a70
Hajnalka Krekuska <krekuskahajni@freemail.hu>
2023-05-28 15:17:38 +0100

    Check user session saga pt 1

--- src/store/user/user.reducer.js
+++ src/store/user/user.reducer.js
@@ -2,7 +2,9 @@ import { USER_ACTION_TYPES } from "./user.types";
 
 //Defining initial state for reducer
 const INITIAL_STATE = {
-    currentUser: null
+    currentUser: null,
+    isLoading: false,
+    error: null,
 }
 
 //Reducer
@@ -10,11 +12,16 @@ export const userReducer = (state = INITIAL_STATE, action) => {
     const { type, payload } = action;
 
     switch(type) {
-        case USER_ACTION_TYPES.SET_CURRENT_USER:
+        case USER_ACTION_TYPES.SIGN_IN_SUCCESS:
             return {
                 ...state,
                 currentUser: payload
-            }
+            };
+        case USER_ACTION_TYPES.SIGN_IN_FAILED:
+            return {
+                ...state, 
+                error: payload
+            };
         default: 
             return state;
     }
