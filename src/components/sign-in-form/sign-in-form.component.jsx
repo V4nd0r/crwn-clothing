9f7b92b9b749201281651b4bfe1c3d88e4118f55
Hajnalka Krekuska <krekuskahajni@freemail.hu>
2023-06-06 08:58:30 +0100

    Sign in with Google

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
 import FormInput from "../form-input/form-input.component";
-import { signInWithGooglePopup, 
-        signInAuthUserWithEmailAndPassword } 
-        from "../../utils/firebase/firebase.utils";
+import { signInAuthUserWithEmailAndPassword } from "../../utils/firebase/firebase.utils";
 import Button, {BUTTON_TYPE_CLASSES} from '../button/button.component';
+import { googleSignInStart } from "../../store/user/user.action";
 import {SignInContainer, 
         ButtonsContainer, 
         Question} 
@@ -15,6 +15,7 @@ const defaultFormFields = {
 }
 
 const SignInForm = () => {
+    const dispatch = useDispatch();
     const [formFields, setFormFields] = useState(defaultFormFields);
     const { email, password } = formFields;
     
@@ -25,7 +26,7 @@ const SignInForm = () => {
     };
 
     const signInWithGoogle = async () => {
-        await signInWithGooglePopup();
+      dispatch(googleSignInStart());  
     }
 
     const handleSubmit = async (event) => {
--- src/store/user/user.saga.js
+++ src/store/user/user.saga.js
@@ -1,7 +1,11 @@
 import { takeLatest, put, all, call } from "redux-saga/effects";
 import { USER_ACTION_TYPES } from "./user.types";
 import { signInSuccess, signInFailed } from "./user.action";
-import { getCurrentUser, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+import { 
+    getCurrentUser, 
+    createUserDocumentFromAuth, 
+    signInWithGooglePopup 
+} from "../../utils/firebase/firebase.utils";
 
 export function* getSnapshotFromUserAuth(userAuth, additionalDetails) {
     try{
@@ -11,6 +15,15 @@ export function* getSnapshotFromUserAuth(userAuth, additionalDetails) {
             additionalDetails
         );
         yield put(signInSuccess({ id: userSnapshot.id, ...userSnapshot.data() }));
+    }catch(error) {
+        yield put(signInFailed(error));
+    }
+}
+
+export function* signInWithGoogle() {
+    try {
+        const{user} = yield call(signInWithGooglePopup);
+        yield call(getSnapshotFromUserAuth, user);
     }catch(error) {
         yield put(signInFailed(error));
     }
@@ -27,10 +40,14 @@ export function* isUserAuthenticated() {
     }
 }
 
+export function* onGoogleSignInStart() {
+    yield takeLatest(USER_ACTION_TYPES.GOOGLE_SIGN_IN_START, signInWithGoogle)
+}
+
 export function* onCheckUserSession() {
     yield takeLatest(USER_ACTION_TYPES.CHECK_USER_SESSION, isUserAuthenticated);
 }
 
 export function* userSagas() {
-    yield all([call(onCheckUserSession)]);
+    yield all([call(onCheckUserSession), call(onGoogleSignInStart)]);
 }
