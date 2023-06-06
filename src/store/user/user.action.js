8f51572dde7ed13644ce029eb2193f0a6e3a1977
Hajnalka Krekuska <krekuskahajni@freemail.hu>
2023-06-06 22:28:50 +0100

    Sign up saga

--- src/store/user/user.action.js
+++ src/store/user/user.action.js
@@ -18,7 +18,17 @@ export const signInSuccess = (user) =>
 
 export const signInFailed = (error) => 
     createAction(USER_ACTION_TYPES.SIGN_IN_FAILED, error);
-/*
+
+export const signUpStart = (email, password, displayName) => 
+    createAction(USER_ACTION_TYPES.SIGN_UP_START, { email, password, displayName });
+
+export const signUpSuccess = (user, additionalDetails) =>
+    createAction(USER_ACTION_TYPES.SIGN_UP_SUCCESS, { user, additionalDetails });
+  
+export const signUpFailed = (error) =>
+    createAction(USER_ACTION_TYPES.SIGN_UP_FAILED, error);
+    
+    /*
     CHECK_USER_SESSION : 'user/CHECK_USER_SESSION',
     GOOGLE_SIGN_IN_START: 'user/GOOGLE_SIGN_IN_START',
     EMAIL_SIGN_IN_START: 'user/EMAIL_SIGN_IN_START',
